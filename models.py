ce806587c612f956dfacbb71093d7b857020cd50
TheFlash98 <mastermind.sarthak@gmail.com>
2017-05-27 21:56:21 +0530

    Some bug fixes

--- models.py
+++ models.py
@@ -9,10 +9,26 @@ def insert_users(username, password):
 
 def insert_admins(username,ssid):
 	con = sql.connect("database.db")
-	cur = con.cursor()
-	cur.execute("INSERT INTO admins (user,ssid) VALUES (?,?)",(username,ssid))
+        flag = True
+	ssidd = ""
+	with con:
+		con.row_factory = sql.Row
+
+        cur = con.cursor()
+        cur.execute("SELECT * FROM admins")
+
+        rows = cur.fetchall()
+        for row in rows:
+        	if row["ssid"]==ssid:
+        		flag = False
+        if (flag):
+	    cur.execute("INSERT INTO admins (user,ssid) VALUES (?,?)",(username,ssid))
+	    message = "New Admin"
+	else:
+	    message = "No New"
 	con.commit()
 	con.close()
+        return(message)
 
 def insert_tempAccess(username,ssid):
 	con = sql.connect("database.db")
